ce9f5ebd6072071d2bff732183e4460715714a1e
Anton <anton@antonlopez.com>
2026-01-14 22:17:58 +0100

    Ruta despliegue produccion

--- testlab/frontend/src/environments/environment.ts
+++ testlab/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost/proyecto/testlab/testlab/backend/public/api'
-    apiUrl: '../backend/public/api'
+    apiUrl: 'backend/public/api'
 };
